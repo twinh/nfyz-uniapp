87b2618772090b23e2cbf34aeb7e58d3b238b9e0
twinh <twinhuang@qq.com>
2021-12-19 22:28:29 +0800

    更新域名地址

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ export default {
     const envVersion = __wxConfig.envVersion;
     let baseApiUrl;
     if (envVersion === "release") {
-      baseApiUrl = "https://nfyz.miaoxz.com/m-api/";
+      baseApiUrl = "https://nfyz.miaoxingyun.com/m-api/";
     } else {
       baseApiUrl = "http://dev.miaoxz.com/m-api/";
     }
